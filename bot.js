f21df673cc7d78a4f13f78cfd34670f71ef451f8
Marcopole11 <35684962+Marcopole11@users.noreply.github.com>
2018-03-22 18:00:33 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,15 +10,13 @@ var comandchat = "comandos"; var dialogchat = "canal-r37j";
 client.on('message', message => {
     if (message.content.startsWith(prefix + 'nuevodirecto') && message.channel.name == dialogchat) {
         let entrada = message.content.split(" ARTEIKA ");
-        let salida = "\n";
         if (entrada[1] == "marcopole"){
-            salida = salida + message.client.guilds.find("name", "Server secreto de Marco").emojis.find("name", "KappaFace") +
+            let salida = salida + message.client.guilds.find("name", "Server secreto de Marco").emojis.find("name", "KappaFace") +
                 "``HEY! ATENCIÓN!``" + message.client.guilds.find("name", "Server secreto de Marco").emojis.find("name", "KappaFace") +
                 "\n **Que Marcopole está en directo!!!**\n" +
                 "Hoy se encuentra " + entrada[3] +
                 "\n A que estás esperando? \n \n Pulsa aqui para unirte: " + entrada[4];
                 salida = salida + "\n Avisando a todos los " + message.guild.roles.find("name", "ad") + " :laughing:";
-        } else {
             message.guild.channels.find("name", "aviso⠐directos").sendMessage(salida);
         }
     } else if (message.content.startsWith(prefix + 'ping')) {

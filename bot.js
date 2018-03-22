38e676c384f03a734be631c3826d5e7a5c96ef6a
Marcopole11 <35684962+Marcopole11@users.noreply.github.com>
2018-03-22 18:36:08 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const servidores = require("./principalDatabase.json");
 
 client.on('ready', () => {
     console.log('I am ready!');

f45ed033ae5d4b0fdd5eae2e57e5a56b1e9e0900
Marcopole11 <35684962+Marcopole11@users.noreply.github.com>
2018-03-22 17:11:10 +0100

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const pluralinfo = require("./principalDatabase.json");
+
+client.on('ready', () => {
+    console.log('I am ready!');
+});
+var prefix = 'm!';
+var comandchat = "comandos"; var dialogchat = "canal-r37j";
+
+client.on('message', message => { 
+
+});
+
+// THIS  IS  THE  WAE
+client.login(process.env.BOT_TOKEN);

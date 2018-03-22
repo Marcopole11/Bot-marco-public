b822e4f020dbddfd408351f7b1daab6ff38061b0
Marcopole11 <35684962+Marcopole11@users.noreply.github.com>
2018-03-22 18:08:44 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,7 +14,7 @@ client.on('message', message => {
             let salida = message.client.guilds.find("name", "Server secreto de Marco").emojis.find("name", "KappaFace") +
                 "``HEY! ATENCIÓN!``" + message.client.guilds.find("name", "Server secreto de Marco").emojis.find("name", "KappaFace") +
                 "\n **Que Marcopole está en directo!!!**\n" +
-                "Hoy se encuentra " + entrada[3] +
+                "Hoy toca " + entrada[3] +
                 "\n A que estás esperando? \n \n Pulsa aqui para unirte: " + entrada[4];
                 salida = salida + "\n Avisando a todos los " + message.guild.roles.find("name", "ad") + " :laughing:";
             message.guild.channels.find("name", "aviso⠐directos").sendMessage(salida);
